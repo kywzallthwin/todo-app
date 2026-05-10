c7a83133974b6cba1a6fc0f620ca39c73d3c3410
kyawzallthwin <56065932+kywzallthwin@users.noreply.github.com>
2026-05-10 16:55:00 +0700

    feat: setup React Router with login and protected todos pages

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+        <div>
+            <h1>Login page</h1>
+            <a href="/">Back to Home</a>
+        </div>
+    )
+}
+
+export default Login;
---
+++ frontend/src/pages/Todos.tsx
@@ -0,0 +1,10 @@
+function Todos() {
+    return (
+        <div>
+            <h1>Todos page</h1>
+            <a href="/">Back to Home</a>
+        </div>
+    )
+}
+
+export default Todos;
